a53a95b66420361e5bf83fe297a30a5d22f35ad9
MattGaiser <matthewgaiser@live.com>
2020-04-21 19:13:33 -0700

    Updates

---
+++ precache-manifest.9bd5dbcb5ada2c9461390a46a600245a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242d5721aa3a52db75585caf7681c4be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7569c378255339c5cdd",
+    "url": "/static/css/2.4fe7acbf.chunk.css"
+  },
+  {
+    "revision": "9152df9f049f3fc1959c",
+    "url": "/static/css/main.419a10c3.chunk.css"
+  },
+  {
+    "revision": "d7569c378255339c5cdd",
+    "url": "/static/js/2.af4e0560.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.af4e0560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9152df9f049f3fc1959c",
+    "url": "/static/js/main.ea2dd3a8.chunk.js"
+  },
+  {
+    "revision": "434f174d30af83ddccaf",
+    "url": "/static/js/runtime-main.cbcdbc53.js"
+  }
+]);
